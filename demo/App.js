4e4e7d6efba1d093617c8006727c1e27ae1c8c63
Quinten Kasteel <kasteel@floydhamilton.nl>
2020-10-30 19:29:12 +0100

    Cleanup and package updates

--- demo/App.js
+++ demo/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-  return null 
+  return <div>actually working mofo?</div> 
 }
 
 ReactDOM.render(<App/>, document.querySelector('#root'));
